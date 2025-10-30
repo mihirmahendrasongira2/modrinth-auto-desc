cee285c330fc021f986f7caa0e7dacb0ba6f18f5
Mihir <champsongira@gmail.com>
2025-10-30 13:50:47 +0530

    feat: Add  demo log

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ import fetch from 'node-fetch';
 import fm from 'yaml-front-matter';
 
 // See: https://docs.modrinth.com/#tag/projects/operation/modifyProject
-
+// demo1 code
+console.log("demo1")
+//Demo code
 const removeExcludedSections = (text) => {
     // Remove sections that are excluded from modrinth description
     // Placeholder: <!-- MODRINTH_EXCLUDE_START --> ... <!-- MODRINTH_EXCLUDE_END -->
